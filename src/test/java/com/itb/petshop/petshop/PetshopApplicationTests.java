e5a5a130e913abd1ed3ef4bb0615bce63f74c228
juliaandrde52@gmail.com <juliaandrde52@gmail.com>
2023-08-22 09:03:18 -0300

    primeiro commit tcc

---
+++ src/test/java/com/itb/petshop/petshop/PetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.petshop.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
